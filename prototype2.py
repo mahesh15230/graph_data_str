a793f3b5559387e5b08a52b184e1ad957afb1327
mahesh15230 <36959431+mahesh15230@users.noreply.github.com>
2018-06-18 19:07:01 +0530

    Add files via upload

--- prototype2.py
+++ prototype2.py
@@ -1,6 +1,9 @@
+#There can't be any repitition in the names of root causes, RCSs, products
+#Tree has to converge for RCSs
+#The tool shows the worst case scenario probability only
+
 class Node:
     nodes = {}
-    
     def __init__(self,nodeName,classname):
         self.name = nodeName
         self.incomingNeighbors = []
@@ -10,9 +13,16 @@ class Node:
         self.nodeweight = 1
         Node.nodes[self.name] = self
         Node.graph[self] = self.outgoingNeighbors
-
+        
+    def getweight(self,node):
+        if node in self.outgoingNeighbors:
+            return Edge.edgesToNode[getNodeName(node)].weight
+        elif node in self.incomingNeighbors:
+            return Edge.edgesFromNode[getNodeName(node)].weight
+        else:
+            print("Invalid Argument supplied")
             
-    def resetnode(self):
+    def reset(self):
         self.isVisited = False
         self.nodeweight = 0
         
@@ -29,31 +39,34 @@ class Node:
                         if not i.isVisited:
                             q.append(i)
                             i.isVisited = True
-                            i.nodeweight *= Edge.edges[s.name+str(":")+i.name].weight * s.nodeweight
+                            i.nodeweight *= Edge.edgesToNode[getNodeName(i)].weight
                             if i.classname == 'product':
                                 print("Outage occurence percentage for", i.name,"is ",100*i.nodeweight,"%")
             else:
                 print("Invalid rootcause")
             for i in Node.nodes.values():
-                i.resetnode()
+                i.reset()
         else:
             if self.classname == 'product':
                 q.append(s)
                 self.isVisited = True
                 while len(q):
                     s = q.pop(0)
-                    for i in s.incomingNeighbors:
+                    for i in self.incomingNeighbors:
                         if not i.isVisited:
                             q.append(i)
                             i.isVisited = True
-                            i.nodeweight *= Edge.edges[i.name+str(":")+s.name].weight * s.nodeweight
-                            if i.classname == 'rootcause':
-                                print("Rootcauses that could causing the outage", i.name,"are ",100*i.nodeweight,"%")
+                            i.nodeweight *= Edge.edgesFromNode[getNodeName(i)].weight
+                            if i.classname == 'product':
+                                print("Outage occurence percentage for", i.name,"is ",100*i.nodeweight,"%")
             else:
                 print("Invalid product")
             for i in Node.nodes.values():
-                i.resetnode()
+                i.reset()
 
+def getNodeName(node):
+    lKey = [key for key, value in Node.nodes.items() if value == node][0]
+    return lKey
 
 def addNode(nodeName,classname):
     nodeAdded = Node(nodeName,classname)
@@ -62,18 +75,25 @@ def resetNode(nodeName):
     Node.nodes[nodeName].isVisited = False
     Node.nodes[nodeName].nodeweight = 0
 
+
+    
+# =============================================================================
+# def setClassName(nodename,classname):
+#     Node.nodes[nodename].classname = classname
+# =============================================================================
+    
 class Edge:
-    edges = {}
+    edgesFromNode = {}
+    edgesToNode = {}
     def __init__(self,fromNode,toNode,weight):
         if fromNode!=toNode:
-            self.name = fromNode.name+str(":")+toNode.name
             self.weight = weight
             self.startNode = fromNode.name
             self.endNode = toNode.name
             Node.nodes[toNode.name].incomingNeighbors.append(Node.nodes[fromNode.name])
             Node.nodes[fromNode.name].outgoingNeighbors.append(Node.nodes[toNode.name])
-            Edge.edges[self.name] = self
-
+            Edge.edgesFromNode[self.startNode] = self
+            Edge.edgesToNode[self.endNode] = self
             
 def addEdge(fromNode,toNode,weight):
     edgeAdded = Edge(fromNode,toNode,weight)
@@ -82,11 +102,37 @@ rootcauses = []
 RCSs = []
 products = []
 
+#A function that asks the user for no of +ve incidents of one thing affecting the other and the -ve incidents affecting the other
+#Find a good way for the user to add conditional probabilities as it gets tedious if the root causes increase.
+#Write another code that records history that's compatible with this code
+#i.e, (Root cause) --> (HL1) --> ... --> (RCS) --> (Product)
+# Basically create a data structure for each incident in history from which conditional probabilities can be calculated.
+#This data structure must be dynamic as root causes, HLs, RCSs and products can be removed and/or added.
+
 class History:
     history = {}
-    def __init__(self,label,rootcause,product,HLs = None): # HLs must be a list
+    def __init__(self,label,rootcause,product):
         self.name = label
         self.rootcause = rootcause
         self.product = product
-        self.hiddenLayer = HLs
+        self.hiddenLayer = []
         History.history[self.name] = self
+
+def recordHistory(label,rootcause,product,HLs): #HLs must be a list.
+    histrec = History(label,rootcause,product)
+    try:
+        histrec.hiddenLayer += list(HLs)
+        History.history[label] = histrec
+        print("Event recorded successfully")
+    except:
+        print("Hidden layers must be passed as a list only")
+
+addNode('a','rootcause')
+addNode('b','rootcause')
+addNode('c','rootcause')
+addNode('d','HL')
+addNode('e','HL')
+addNode('f','product')
+addNode('g','product')
+addNode('h','product')
+addNode('i','product')
