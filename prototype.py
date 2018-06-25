7262bbec9f5307a678db4e9255d33bc0c7b111b0
mahesh15230 <36959431+mahesh15230@users.noreply.github.com>
2018-06-25 12:51:05 +0530

    Add files via upload

--- prototype.py
+++ prototype.py
@@ -13,11 +13,11 @@ import os
 class Node:
     nodes = {}
     node_id = {}
-    def __init__(self,nodeName,classname):
+    def __init__(self,nodeName,classname,incneigh=None,outneigh=None,id=None):
         self.name = nodeName
         self.id = len(Node.nodes)
-        self.incomingNeighbors = []
-        self.outgoingNeighbors = []
+        self.incomingNeighbors = [] # List of strings
+        self.outgoingNeighbors = [] # List of strings
         self.classname = classname # Can be 'rootcause','HL','RCS' or 'product'
         self.isVisited = False
         self.nodeweight = 1
@@ -30,7 +30,7 @@ class Node:
         self.nodeweight = 0
         
     def bfs(self, toggle = None): # toggle == None is rootcause to products
-        s = self
+        s = self.name
         q = []
         if toggle == None:
             if self.classname == 'rootcause':
@@ -38,13 +38,13 @@ class Node:
                 self.isVisited = True
                 while len(q):
                     s = q.pop(0)
-                    for i in s.outgoingNeighbors:
-                        if not i.isVisited:
+                    for i in Node.nodes[s].outgoingNeighbors:
+                        if not Node.nodes[i].isVisited:
                             q.append(i)
-                            i.isVisited = True
-                            i.nodeweight *= Edge.edges[s.name+str(":")+i.name].weight * s.nodeweight
-                            if i.classname == 'product':
-                                print("Outage occurence percentage for", i.name,"is ",100*i.nodeweight,"%")
+                            Node.nodes[i].isVisited = True
+                            Node.nodes[i].nodeweight *= Edge.edges[s+":"+i].weight * Node.nodes[s].nodeweight
+                            if Node.nodes[i].classname == 'product':
+                                print("Outage occurence percentage for", i,"is ",100*Node.nodes[i].nodeweight,"%")
             else:
                 print("Invalid rootcause")
             for i in Node.nodes.values():
@@ -55,13 +55,13 @@ class Node:
                 self.isVisited = True
                 while len(q):
                     s = q.pop(0)
-                    for i in s.incomingNeighbors:
-                        if not i.isVisited:
+                    for i in Node.nodes[s].incomingNeighbors:
+                        if not Node.nodes[i].isVisited:
                             q.append(i)
-                            i.isVisited = True
-                            i.nodeweight *= Edge.edges[i.name+str(":")+s.name].weight * s.nodeweight
-                            if i.classname == 'rootcause':
-                                print("Rootcauses that could causing the outage", i.name,"are ",100*i.nodeweight,"%")
+                            Node.nodes[i].isVisited = True
+                            Node.nodes[i].nodeweight *= Edge.edges[i+":"+s].weight * Node.nodes[s].nodeweight
+                            if Node.nodes[i].classname == 'rootcause':
+                                print("Rootcauses that could causing the outage", i,"are ",100*Node.nodes[i].nodeweight,"%")
             else:
                 print("Invalid product")
             for i in Node.nodes.values():
@@ -73,47 +73,80 @@ class Node:
 #     nodeAdded = Node(nodeName,classname)
 # =============================================================================
     
-def delNode(nodeName):
-    for i in Edge.edges.keys():
-        if nodeName+":" in i:
-            Edge.edges[None] = Edge.edges.pop(nodeName)
-            Edge.edges[i].startNode = None
-        elif ":"+nodeName in i:
-            Edge.edges[None] = Edge.edges.pop(nodeName)
-            Edge.edges[i].endNode = None
-    del Node.nodes[nodeName]
-    print(nodeName,"is deleted")
+def delNode(nodeName,toggle=None):
+    if toggle == None:  
+        for i in Edge.edges.keys():
+            if nodeName+":" in i:
+                Edge.edges[i].startNode = None
+                Edge.edges['None'+str(Edge.edges[i].endNode)] = Edge.edges.pop(i)
+            elif ":"+nodeName in i:
+                Edge.edges[i].endNode = None
+                Edge.edges[str(Edge.edges[i].startNode)+'None'] = Edge.edges.pop(i)
+        del Node.nodes[nodeName]
+        print(nodeName,"is deleted")
+    else:
+        for i in Edge.edges.keys():
+            if nodeName in i:
+                del Edge.edges[i]
+        del Node.nodes[nodeName]
+        print("Node along with attached edges are deleted")
     
-def delEdge(fromNodename, toNodename):
-    del Edge.edges[fromNodename+":"+toNodename]
+def delEdge(id):
+    del Edge.edge_id[id]
     print("Edge is deleted")
     
 class Edge:
     edges = {}
     edge_id = {}
-    def __init__(self,fromNode,toNode,weight):
-        if fromNode!=toNode:
-            self.id = len(Edge.edges)
-            self.name = fromNode.name+str(":")+toNode.name
-            self.weight = weight
-            self.startNode = fromNode.name
-            self.endNode = toNode.name
-            Node.nodes[toNode.name].incomingNeighbors.append(Node.nodes[fromNode.name])
-            Node.nodes[fromNode.name].outgoingNeighbors.append(Node.nodes[toNode.name])
-            Edge.edges[self.name] = self
-            Edge.edges[self.id] = self
+    def __init__(self,id,fromNode,toNode,weight,name=None): # Except for weight and id everything else is a string
+        if name == None:    
+            if fromNode!=toNode:
+                self.id = len(Edge.edges)
+                self.name = fromNode+":"+toNode
+                self.weight = weight
+                self.startNode = fromNode
+                self.endNode = toNode
+                Node.nodes[toNode].incomingNeighbors.append(fromNode)
+                Node.nodes[fromNode].outgoingNeighbors.append(toNode)
+                Edge.edges[self.name] = self
+                Edge.edges[self.id] = self
+            else:
+                print("Error : Edge can't be created")
+        else:
+            if fromNode!=toNode:
+                self.id = len(Edge.edges)
+                self.name = name
+                self.weight = weight
+                self.startNode = fromNode
+                self.endNode = toNode
+                Node.nodes[toNode].incomingNeighbors.append(fromNode)
+                Node.nodes[fromNode].outgoingNeighbors.append(toNode)
+                Edge.edges[self.name] = self
+                Edge.edges[self.id] = self
+            else:
+                print("Error : Edge can't be created")
+    
+    def get_id(self,toNode=None,fromNode=None):
+        a = []
+        if toNode == None and fromNode == None:
+            return self.id
+        elif toNode == None and fromNode != None:
+            for i in Edge.edges.keys():
+                if fromNode+':' in i:
+                    a.append(Edge.edges[i].id)
+            return a
+        else:
+            for i in Edge.edges.keys():
+                if ":"+toNode in i:
+                    a.append(Edge.edges[i].id)
+            return a
 
-class History:
-    history = {}
-    def __init__(self,label,rootcause,product,HLs = None): # HLs must be a list
-        self.name = label
-        self.rootcause = rootcause
-        self.product = product
-        self.hiddenLayer = HLs
-        History.history[self.name] = self
+
+
 # =============================================================================
 app = Flask(__name__)
 app.secret_key = os.urandom(16)
+
 
 @app.route('/')
 def loginpage():
@@ -127,12 +160,32 @@ def login(username,pwd):
         return render_template('prelogin.html', default = 1) # In html page if default is 1 it says invalid credentials. Otherwise it says please login
 @app.route('/<int:option>')
 def selectfn(option):
+    try:
+      cursor.execute("select * from node;")
+      nodes = cursor.fetchall()
+      for node in nodes:
+          Node(node[1], node[2])
+    except:
+        print 'Failed to fetch nodes'
+
+    try:
+      cursor.execute("select * from edge;")
+      edges = cursor.fetchall()
+      for edge in edges:
+          Edge(edge[2], edge[3], edge[4])
+    except:
+        print 'Failed to fetch edges'
+
+
     #options would be addnode, addedge, do bfs forward direction, do bfs reverse direction,addhistory
     #redirect to respective html page
 @app.route("/addnode/<str:classname>/<str:nodename>",methods = ['GET','POST'])
 def addNode(nodeName,classname):
     nodeAdded = Node(nodeName,classname)
     #send this to database
+    insertcmd = "insert into node values(%d, %s, %s, %s, %s);"%(nodeAdded.id, nodeAdded.name, nodeAdded.classname, nodeAdded.incomingNeighbors, nodeAdded.outgoingNeighbors);
+    cursor.execute(insertcmd);
+
     #add the same to the session object
     return render_template("addnode.html", nodeName=nodeName, classname=classname)
 
@@ -141,11 +194,13 @@ def addEdge(startnodename,endnodename,weight):
     if startnodename in Node.nodes.keys() and endnodename in Node.nodes.keys():
         edgeAdded = Edge(startnodename,endnodename,weight)
         #Send this to database
+        insertcmd = "insert into edge values(%d, %s, %d, %d, %d);"%(edgeAdded.edge_id, edgeAdded.name, edgeAdded.startNode, edgeAdded.endNode, edgeAdded.weight);
+        cursor.execute(insertcmd)
         #Update incomingneigh, outgoingneigh column values of start and end nodes
         return render_template('addedge.html', startnode=startnodename, endnode=endnodename)
     else:
         return "!!!Error!!!"
-    
+
 @app.route('/bfsrootcause/<str:rootcause>', methods = [])
 def bfsF(rootcause):
     if rootcause in Node.nodes.keys():
@@ -161,10 +216,3 @@ def bfsR(product):
     else:
         print('!!!Error!!!')
     return render_template('bfsR.html',product=product)
-        
-        
-        
-        
-        
-        
-   
